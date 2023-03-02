a452fa5b4fea87b8618af16fb243693c0ec46327
ezdilod <97134175+ezdilod@users.noreply.github.com>
2023-03-02 10:58:02 +0100

    init Repo

---
+++ passwordGenerator.py
@@ -0,0 +1,33 @@
+
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+random_let = random.choices(letters, k=nr_letters)
+random_sym = random.choices(symbols, k=nr_symbols)
+random_num = random.choices(numbers, k=nr_numbers)
+
+print(random_let + random_sym + random_num)
+
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+random_password_list = random_let + random_sym + random_num
+random_password = random.sample(
+    random_password_list, k=len(random_password_list))
+
+password_string = ''.join(random_password)
+
+print(password_string)
